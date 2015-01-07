adec0e5fec8c145f932a50139f09932f269b6f22
Artur Moczulski <artur.moczulski@gmail.com>
2015-01-07 04:53:27 -0800

    Added unit test framework

---
+++ tests/CreditCardsCom/Exercises/Exercise1/ProductTest.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+ * Artur Moczulski <artur.moczulski@gmail.com>
+ */
+
+namespace CreditCardsCom\Tests;
+
+class ProductTest extends \PHPUnit_Framework_TestCase
+{
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+ * Artur Moczulski <artur.moczulski@gmail.com>
+ *
+ * Bootstrap file for PHPUnit tests for creditcards.com assignments.
+ */
+
+require_once(dirname(__FILE__)."/../vendor/autoload.php");
+
